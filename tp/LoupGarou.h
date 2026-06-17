b36790e877897b4c23c144b5b5ff6135df84d6e8
Garmin <louis.guerin17220@gmail.com>
2026-06-17 14:43:44 +0200

    creation de la base du jeu

---
+++ tp/LoupGarou.h
@@ -0,0 +1,14 @@
+#ifndef loups_garou_h
+#define loups_garou_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char nom[50];
+    char role[20];
+    int vote;
+} Joueur;
+
+#endif
